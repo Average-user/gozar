e812b1bd35863694e7bd63bde93656b4452c61b7
Average-user <lucaspolymeris@pm.me>
2018-09-21 22:57:12 -0300

    Board size generalization and Game example

--- js/compiled/out/devtools/version.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.9.542 {}
-goog.provide('devtools.version');
-goog.require('cljs.core');
-devtools.version.current_version = "0.8.2";
-devtools.version.get_current_version = (function devtools$version$get_current_version(){
-return devtools.version.current_version;
-});
-
-//# sourceMappingURL=version.js.map?rel=1537411816262
--- js/compiled/out/gozar/config.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.9.542 {}
-goog.provide('gozar.config');
-goog.require('cljs.core');
-gozar.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=config.js.map?rel=1537411813546
